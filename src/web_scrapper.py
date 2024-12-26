fe850acfb7c186084f076cd76ad482785cd5ecb6
aexpedito <aexpedito@hotmail.com>
2024-12-26 15:44:07 -0300

    Fixed some warnings

--- src/web_scrapper.py
+++ src/web_scrapper.py
@@ -1,11 +1,74 @@
-from abc import ABC, abstractmethod
+"""
+Some docstring for validation pass.
+This is a web_scrapper for.
+"""
+from abc import abstractmethod
+
 from singleton import Singleton
+
 
 #
 class Web_Scrapper(Singleton):
-
     """
-        Abstract class for a Web Scrapper
+        Abstract class for a Web Scrapper.
+
+        Args:
+            None
+        Returns:
+            None
+    """
+    @abstractmethod
+    def config(self) -> None:
+        """
+            Abstract class for a Web Scrapper.
+
+            Args:
+                a
+            Returns:
+                None
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def grab(self) -> None:
+        """
+            Abstract class for a Web Scrapper.
+
+            Args:
+                None
+            Returns:
+                None
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def persist(self) -> None:
+        """
+            Abstract class for a Web Scrapper.
+
+            Args:
+                None
+            Returns:
+                None
+        """
+        raise NotImplementedError
+
+    def scrapping(self) -> None:
+        """
+            Abstract class for a Web Scrapper.
+
+            Args:
+                None
+            Returns:
+                None
+        """
+        self.captar()
+        self.persist()
+
+
+class Web_Scrapper_Factory(Singleton):
+    """
+        Abstract class for a Web Scrapper.
 
         Args:
             a
@@ -13,24 +76,13 @@ class Web_Scrapper(Singleton):
             None
     """
     @abstractmethod
-    def config(self) -> None:
-        raise NotImplementedError
-
-    @abstractmethod
-    def grab(self) -> None:
-        raise NotImplementedError
-
-    @abstractmethod
-    def persist(self) -> None:
-        raise NotImplementedError
-
-    def scrapping(self) -> None:
-        self.captar()
-        self.persist()
-
-
-class Web_Scrapper_Factory(Singleton):
-
-    @abstractmethod
     def create(self) -> Web_Scrapper:
+        """
+            Abstract class for a Web Scrapper.
+
+            Args:
+                None
+            Returns:
+                Web_Scrapper
+        """
         raise NotImplementedError
